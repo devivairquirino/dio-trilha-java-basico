f06f8085bf9a428a9cbd6ead05840a772dfbcda6
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-05-20 22:11:19 -0300

    Implementa a funcionalidade de cadastro e exibição de dados da conta no ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        // instacia class scanner para ler dados do usuario
+        Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
+
+        // programa espera usuario insirir valor para prosseguir
+        System.out.print("Por favor, digite o número da Agência!  ");
+        String agencia= scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta!  ");
+        int conta= scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite seu nome!  ");
+        String nome= scanner.nextLine();
+
+        System.out.print("Por favor, digite o valor que deseja depositar!  ");
+        Double saldo= scanner.nextDouble();
+
+        scanner.close();
+        
+        exibirDadosDaConta(nome,agencia,conta,saldo);
+        
+    }
+    // metodo exibe os dados cadastrados
+    public static void exibirDadosDaConta(String nome, String agencia, int conta, Double saldo){
+        // formatar o valor do saldo
+            Locale brasil = Locale.forLanguageTag("pt-BR");
+            NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(brasil);
+            
+        System.out.print("\nOlá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta é "+conta+" e seu saldo "+formatoMoeda.format(saldo)+" já está disponível para saque.\n");
+
     }
 }
