c2e8497288e955c5b341337b6851cffab9295909
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-05-27 12:43:48 -0300

    Adiciona a classe Student com atributos nome, idade e sexo, além de métodos de acesso e ação

---
+++ ControleDeFluxo/src/Class.java
@@ -0,0 +1,8 @@
+public class Class {
+    public static void main(String[] args) {
+        Student aluno = new Student();
+        aluno.setNome("Ivair");
+
+        System.out.println(aluno.getNome());
+    }
+}
---
+++ ControleDeFluxo/src/Student.java
@@ -0,0 +1,33 @@
+public class Student {
+    private String nome;
+    private int idade;
+    private char sexo;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    void andar() {
+        System.out.println("Andou");
+    }
+}
